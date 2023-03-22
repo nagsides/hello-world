ff40c1e67813fd01aa31f9e59ee30a789b6fbd80
nagsides <nags.ides@gmail.com>
2023-03-22 21:04:11 +0530

    modified Program.cs and Startup.cs for namespace resolution

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-namespace HelloWorld
+namespace hello_world
 {
   class Program
   {
