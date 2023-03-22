e20d7e24202ffc529c16f3e0883c7844fa6dc764
nagsides <nags.ides@gmail.com>
2023-03-22 20:50:59 +0530

    added Startup.cs and modified Program.cs file for web based application running on 5000

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using System;
-namespace ConsoleApplication
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+namespace HelloWorld
 {
-  class Program{
+  class Program
+  {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var config = new ConfigurationBuilder()
+	      .AddCommandLine(args)
+	      .Build();
+
+      var host = new WebHostBuilder()
+	      .UseKestrel()
+	      .UseStartup<Startup>()
+	      .UseConfiguration(config)
+	      .Build();
+      
+      host.Run();
     }
   }
 }
